39769a56d098af75bc006d89331082d2d7aac15a
CockroachCommunist <ahmednagucev@gmail.com>
2022-03-04 16:02:47 +0300

    Adding an error reporting component

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Competition, LeagueMatch, TeamMatch, Teams } from "./components";
 import { Routes, Route } from "react-router-dom";
 import { Error } from "./components/Error/Error";
+import { RootState } from "./store/rootReducer";
+import { useSelector } from "react-redux";
 
 const Content = () => {
+  const teamId = useSelector((state: RootState) => state.teamMatchId.openId);
+  const leagueId = useSelector(
+    (state: RootState) => state.leagueMatchId.openId
+  );
   return (
     <>
       <Routes>
@@ -11,7 +17,8 @@ const Content = () => {
         <Route path="/SoccerStat/:id/match" element={<LeagueMatch />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/teams/:id/match" element={<TeamMatch />} />
-        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Error />} />
+        {/* {!leagueId && <Route path="*" element={<Error />} />} */}
       </Routes>
     </>
   );
